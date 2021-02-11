e3346527c47f7146e5747143263a2be9af714fed
Diogo-Valerio <34871369+Diogo-Valerio@users.noreply.github.com>
2021-02-10 21:15:24 -0300

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+$(function(){
+
+ 
+  $(".txtb input").on("focus",function(){
+    $(this).addClass("focus");
+  });
+
+  $(".txtb input").on("blur",function(){
+    if($(this).val() == "")
+    $(this).removeClass("focus");
+  });
+
+  
+
+
+});
